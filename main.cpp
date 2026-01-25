294ad083df5b23840ee2517037ba1831d652ffbc
eltahir <conan2308.edo@gmail.com>
2026-01-24 20:33:17 -0500

    linear allocator done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <stdint.h>
+#include <cassert>
 
+struct AllocationHeader {
+    size_t block_size; // size requested by user
+    uint8_t padding;
+};
+
+// padding - header - user memory
 
 class LinearAllocator {
 
@@ -15,16 +22,33 @@ public:
     : memory(ptr), capacity(size), offset(0) {
     }
 
-    void* alloc(size_t size) {
+    void* alloc(size_t size, size_t alignment) {
 
-        if (offset + size > capacity) return nullptr;
+        assert((alignment != 0) && ((alignment & (alignment - 1)) == 0) && "alignment must be a power of 2");
 
-        void* ptr = memory + offset;
+        void* current_address = memory + offset + sizeof(AllocationHeader);
 
-        offset += size;
+        uintptr_t addr = (uintptr_t)current_address;
+        uintptr_t aligned_addr = (addr + alignment - 1) & ~(alignment - 1); // gets the aligned address
 
-        return ptr;
+        uintptr_t padding = aligned_addr - addr; // padding
 
+        if (offset + sizeof(AllocationHeader) + padding + size > capacity) return nullptr;
+
+        offset += sizeof(AllocationHeader) + padding + size;
+
+        AllocationHeader* header = (AllocationHeader*) (aligned_addr - sizeof(AllocationHeader));
+        header->block_size = size;
+        header->padding = padding; //adjustment
+
+        return (void*)aligned_addr;
+
+    }
+
+    void free(void* ptr) {
+        AllocationHeader* header = (AllocationHeader*)((uint8_t*)ptr - sizeof(AllocationHeader));
+        std::cout << "block_size is " << header->block_size << std::endl;
+        std::cout << "padding is " << +header->padding << std::endl;
     }
 
     void reset() {
@@ -48,39 +72,23 @@ int main() {
 
     std::cout << "avaliable memoery : " << allocator.get_available() << std::endl;
 
-    int* num1 = static_cast<int*>(allocator.alloc(sizeof(int)));
-    if (num1 != nullptr) {
-        *num1 = 42;
-        std::cout << " allocated at: " << static_cast<void*>(num1) << std::endl;
-        std::cout << " value stored: " << *num1 << std::endl;
-        std::cout << " memory used: " << allocator.get_used() << " bytes" << std::endl;
-    }
-
-    int* num2 = static_cast<int*>(allocator.alloc(sizeof(int)));
-    if (num2 != nullptr) {
-        *num1 = 69;
-        std::cout << " allocated at: " << static_cast<void*>(num2) << std::endl;
-        std::cout << " value stored: " << *num2 << std::endl;
-        std::cout << " memory used: " << allocator.get_used() << " bytes" << std::endl;
-    }
-
-    char* c = static_cast<char*>(allocator.alloc(sizeof(char)));
+    char* c = static_cast<char*>(allocator.alloc(sizeof(char), 4));
     if (c != nullptr) {
         *c = 'X';
         std::cout << " allocated at: " << static_cast<void*>(c) << std::endl;
         std::cout << " value stored: " << *c << std::endl;
         std::cout << " memory used: " << allocator.get_used() << " bytes" << std::endl;
+        allocator.free(c);
     }
 
-    // allocate too much
-
-    std::cout << " available: " << allocator.get_available() << std::endl;
-    void* too_much = allocator.alloc(2000);
-    if (too_much == nullptr) {
-        std::cout << "too much!!" << std::endl;
-    } else {
-        std::cout << "shouldnt happen" << std::endl;
+    int* num1 = static_cast<int*>(allocator.alloc(sizeof(int), 4));
+    if (num1 != nullptr) {
+        *num1 = 42;
+        std::cout << " allocated at: " << static_cast<void*>(num1) << std::endl;
+        std::cout << " value stored: " << *num1 << std::endl;
+        std::cout << " memory used: " << allocator.get_used() << " bytes" << std::endl;
+        allocator.free(num1);
     }
-    
+
     return 0;
 }
